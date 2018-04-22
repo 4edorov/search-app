492ad2a8e0cc167657e4a826c65c8505ff99d54f
4edorov <4edorov@gmail.com>
2018-04-22 12:16:34 +0300

    set CSE

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const debug = require('debug-levels')('server')
 const DbConn = require('./server/lib/DbConn')
+const https = require('https')
 
 const app = express()
 
@@ -17,6 +18,44 @@ app.get('/api/imagesearch/*', async (req, res) => {
     res.json({error: 'there is no query'})
     return
   }
+
+  const historyData = {
+    query,
+    careatedAt: new Date()
+  }
+
+  const coll = DbConn.getColl()
+  await coll.insert(historyData)
+
+  const searchEngineUri = `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_CX}&q=${query}&searchType=image&num=${offset}`
+  https.get(searchEngineUri, response => {
+    let data = ''
+
+    response.on('data', chunk => {
+      data += chunk
+    })
+
+    response.on('end', () => {
+      try {
+        const parsedData = JSON.parse(data)
+        debug.debug('data', parsedData)
+        res.json(parsedData)
+      } catch (error) {
+        debug.error('ERROR', error)
+      }
+    })
+  }).on('error', error => {
+    debug.error('ERROR', error)
+  })
+})
+
+app.get('/api/latest/imagesearch', async (req, res) => {
+  const coll = DbConn.getColl()
+  const mongoRes = await coll.find().sort({createdAt: 1}).limit(10)
+  const latestQueries = await mongoRes.toArray()
+  debug.debug('latestQueries', latestQueries)
+
+  res.json(latestQueries)
 })
 
 async function startUp() {
